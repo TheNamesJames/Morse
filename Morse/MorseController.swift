427dd0bc529abc91667352805b69de088493554f
James Wilkinson <james.wilkinson@pagesuite.co.uk>
2017-01-28 11:09:01 +0000

    Replace morse code storage with hard coded Morse enum values (better abstraction between string/char to enum)

--- Morse/MorseController.swift
+++ Morse/MorseController.swift
@@ -8,60 +8,70 @@
 
 import Foundation
 
-private let morseCodes = [
-    "0": "-----",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "a": ".-",
-    "b": "-...",
-    "c": "-.-.",
-    "d": "-..",
-    "e": ".",
-    "f": "..-.",
-    "g": "--.",
-    "h": "....",
-    "i": "..",
-    "j": ".---",
-    "k": "-.-",
-    "l": ".-..",
-    "m": "--",
-    "n": "-.",
-    "o": "---",
-    "p": ".--.",
-    "q": "--.-",
-    "r": ".-.",
-    "s": "...",
-    "t": "-",
-    "u": "..-",
-    "v": "...-",
-    "w": ".--",
-    "x": "-..-",
-    "y": "-.--",
-    "z": "--..",
-    ".": ".-.-.-",
-    ",": "--..--",
-    "?": "..--..",
-    "!": "-.-.--",
-    "-": "-....-",
-    "/": "-..-.",
-    "@": ".--.-.",
-    "(": "-.--.",
-    ")": "-.--.-",
-    " ": "/"
-]
-
 class MorseController {
+    
+    private static let morse: [Character: [Morse]] = [
+        "0": [.dash, .dash, .dash, .dash, .dash],
+        "1": [.dot, .dash, .dash, .dash, .dash],
+        "2": [.dot, .dot, .dash, .dash, .dash],
+        "3": [.dot, .dot, .dot, .dash, .dash],
+        "4": [.dot, .dot, .dot, .dot, .dash],
+        "5": [.dot, .dot, .dot, .dot, .dot],
+        "6": [.dash, .dot, .dot, .dot, .dot],
+        "7": [.dash, .dash, .dot, .dot, .dot],
+        "8": [.dash, .dash, .dash, .dot, .dot],
+        "9": [.dash, .dash, .dash, .dash, .dot],
+        "a": [.dot, .dash],
+        "b": [.dash, .dot, .dot, .dot],
+        "c": [.dash, .dot, .dash, .dot],
+        "d": [.dash, .dot, .dot],
+        "e": [.dot],
+        "f": [.dot, .dot, .dash, .dot],
+        "g": [.dash, .dash, .dot],
+        "h": [.dot, .dot, .dot, .dot],
+        "i": [.dot, .dot],
+        "j": [.dot, .dash, .dash, .dash],
+        "k": [.dash, .dot, .dash],
+        "l": [.dot, .dash, .dot, .dot],
+        "m": [.dash, .dash],
+        "n": [.dash, .dot],
+        "o": [.dash, .dash, .dash],
+        "p": [.dot, .dash, .dash, .dot],
+        "q": [.dash, .dash, .dot, .dash],
+        "r": [.dot, .dash, .dot],
+        "s": [.dot, .dot, .dot],
+        "t": [.dash],
+        "u": [.dot, .dot, .dash],
+        "v": [.dot, .dot, .dot, .dash],
+        "w": [.dot, .dash, .dash],
+        "x": [.dash, .dot, .dot, .dash],
+        "y": [.dash, .dot, .dash, .dash],
+        "z": [.dash, .dash, .dot, .dot],
+        ".": [.dot, .dash, .dot, .dash, .dot, .dash],
+        ",": [.dash, .dash, .dot, .dot, .dash, .dash],
+        "?": [.dot, .dot, .dash, .dash, .dot, .dot],
+        "'": [.dot, .dash, .dash, .dash, .dash, .dot],
+        "!": [.dash, .dot, .dash, .dot, .dash, .dash],
+        "/": [.dash, .dot, .dot, .dash, .dot],
+        "(": [.dash, .dot, .dash, .dash, .dot, .dash],
+        ")": [.dash, .dot, .dash, .dash, .dot, .dash],
+        "&": [.dot, .dash, .dot, .dot, .dot],
+        ":": [.dash, .dash, .dash, .dot, .dot, .dot],
+        ";": [.dash, .dot, .dash, .dot, .dash, .dot],
+        "=": [.dash, .dot, .dot, .dot, .dash],
+        "+": [.dot, .dash, .dot, .dash, .dot],
+        "-": [.dash, .dot, .dot, .dot, .dot, .dash],
+        "_": [.dot, .dot, .dash, .dash, .dot, .dash],
+        "\"": [.dot, .dash, .dot, .dot, .dash, .dot],
+         "$": [.dot, .dot, .dot, .dash, .dot, .dot, .dash],
+         "@": [.dot, .dash, .dash, .dot, .dash, .dot],
+         " ": [.space]
+    ]
+    
     private static let shared = MorseController()
     
     private(set) static var validCharacters: CharacterSet = {
-        let uniqueValidChars = Set<String>(morseCodes.keys + morseCodes.keys.map { $0.uppercased() })
+        let uniqueValidChars = Set<String>(morse.keys.map { String($0) } + morse.keys.map { String($0).uppercased() })
         return CharacterSet(charactersIn: uniqueValidChars.joined())
     }()
     
@@ -96,9 +106,8 @@ class MorseController {
     }
     
     class func morseString(from string: String) -> String? {
-        return morse(from: string)?
-            .map { String($0.map { $0.rawValue }) }
-            .joined(separator: " ")
+        guard let morse = morse(from: string) else { return nil }
+        return morseString(from: morse)
     }
     
     class func morse(from string: String) -> [[Morse]]? {
@@ -108,8 +117,13 @@ class MorseController {
         }
         
         return validated.validatedString.lowercased().characters
-            .flatMap { morseCodes[String($0)] }
-            .flatMap { $0.characters.flatMap { Morse(rawValue: $0) } }
+            .flatMap { morse[$0] }
+    }
+    
+    class func morseString(from morse: [[Morse]]) -> String {
+        return morse
+            .map { String($0.map { $0.rawValue }) }
+            .joined(separator: " ")
     }
     
     class func transmit(_ string: String, block: @escaping (Bool) -> Void, reset: @escaping () -> Void) -> TimerInvalidatorBlock? {
